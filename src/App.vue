1acc53ee1b2ed1eadd02bb64390f922248acb88d
YZH <yzh@YZHdeMacBook-Air.local>
2018-05-28 11:37:41 +0800

    vuemusic

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <MHeader></MHeader>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  }
+}
+</script>
+<style lang="stylus" scoped>
+</style>
